108c02baf726d496109a91cf6f9c0abe4b1617be
Ionut Prilipceanu <prilipceanu.ionut@gmail.com>
2023-06-19 08:03:00 +0300

    remove loader

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,18 +1,21 @@
-import React from "react";
-import { Triangle } from "react-loader-spinner";
+import React from "react"
+// import { Triangle } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <Triangle
-      height="80"
-      width="80"
-      color="rgb(0, 0, 0, 0.5)"
-      ariaLabel="triangle-loading"
-      wrapperStyle={{ margin: "20rem" }}
-      wrapperClassName=""
-      visible={true}
-    />
-  );
-};
+    // <Triangle
+    //   height="80"
+    //   width="80"
+    //   color="rgb(0, 0, 0, 0.5)"
+    //   ariaLabel="triangle-loading"
+    //   wrapperStyle={{ margin: "20rem" }}
+    //   wrapperClassName=""
+    //   visible={true}
+    // />
+    <div className="fa-3x">
+      <i className="fas fa-spinner fa-pulse"></i>
+    </div>
+  )
+}
 
-export default Loader;
+export default Loader
