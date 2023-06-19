e819e86290485a09dcb165563338eeccb34311e6
Ionut Prilipceanu <prilipceanu.ionut@gmail.com>
2023-06-19 18:37:59 +0300

    add contact field

--- src/components/contactForm/FormTemp.js
+++ src/components/contactForm/FormTemp.js
@@ -1,27 +1,63 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "react-bootstrap/Button"
 
 const FormTemp = () => {
+  const [startDate, setStartDate] = useState(new Date())
   return (
     <div>
       <form name="Contact Form" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="Contact Form" />
-        <p>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
-        </p>
-        <p>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" />
-        </p>
-        <p>
+        <div className="sections">
+          <div className="form-group">
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div>
+              <label htmlFor="phoneNumber">Telefon:</label>
+              <input
+                type="number"
+                name="phoneNumber"
+                id="phoneNumber"
+                className="form-input"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" id="email" />
+            </div>
+          </div>
+          <div className="form-group">
+            <div>
+              <label htmlFor="eventName">Tip eveniment:</label>
+              <input type="text" name="eventName" id="eventName" />
+            </div>
+            <div>
+              <label htmlFor="phone">Data:</label>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                className="form-input"
+                selected={startDate}
+                onChange={date => setStartDate(date)}
+                format="DD/MM/YY"
+              />
+            </div>
+            <div>
+              <label htmlFor="city">Oraș:</label>
+              <input type="city" name="city" id="city" />
+            </div>
+          </div>
+        </div>
+        <div className="form-group">
           <label htmlFor="comments">Comment:</label>
           <br />
           <textarea name="comments" id="comments"></textarea>
-        </p>
-        <Button type="submit" variant="dark">
-          Send
-        </Button>
+          <Button type="submit" variant="dark">
+            Send
+          </Button>
+        </div>
       </form>
     </div>
   )
--- src/pages/Cookies.js
+++ src/pages/Cookies.js
@@ -1,21 +1,16 @@
 import React from "react"
 import Title from "..//components/title/Title"
-// import styled from "styled-components"
 import Navbarmenu from "..//components/navbarmenu/Navbarmenu"
 import Footer from "..//components/footer/Footer"
+import "..//components/style.css"
 
-// const CostumStyle = styled.div`
-//   ::before {
-//     display: none;
-//   }
-// `
 export default function Portfolio() {
   return (
     <div>
       <Navbarmenu />
-      {/* <CostumStyle> */}
-      <Title title="Cookies" />
-      {/* </CostumStyle> */}
+      <div className="displayNone">
+        <Title title="Cookies" />
+      </div>
       <Footer />
     </div>
   )
